5afd7b1b5ae418ac10a8556105dc1acd62ba06e8
estrellasdebetta <129418767+estrellasdebetta@users.noreply.github.com>
2023-03-30 19:54:05 +0200

    ok

---
+++ web.archive.org/web/20170811145328js_/http_/estrellasdebetta.com/wp-content/plugins/woocommerce/assets/js/js-cookie/js.cookie.min6b25.js
@@ -0,0 +1,42 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*!
+ * JavaScript Cookie v2.1.4
+ * https://github.com/js-cookie/js-cookie
+ *
+ * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
+ * Released under the MIT license
+ */
+!function(a){var b=!1;if("function"==typeof define&&define.amd&&(define(a),b=!0),"object"==typeof exports&&(module.exports=a(),b=!0),!b){var c=window.Cookies,d=window.Cookies=a();d.noConflict=function(){return window.Cookies=c,d}}}(function(){function a(){for(var a=0,b={};a<arguments.length;a++){var c=arguments[a];for(var d in c)b[d]=c[d]}return b}function b(c){function d(b,e,f){var g;if("undefined"!=typeof document){if(arguments.length>1){if(f=a({path:"/"},d.defaults,f),"number"==typeof f.expires){var h=new Date;h.setMilliseconds(h.getMilliseconds()+864e5*f.expires),f.expires=h}f.expires=f.expires?f.expires.toUTCString():"";try{g=JSON.stringify(e),/^[\{\[]/.test(g)&&(e=g)}catch(a){}e=c.write?c.write(e,b):encodeURIComponent(String(e)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,decodeURIComponent),b=encodeURIComponent(String(b)),b=b.replace(/%(23|24|26|2B|5E|60|7C)/g,decodeURIComponent),b=b.replace(/[\(\)]/g,escape);var i="";for(var j in f)f[j]&&(i+="; "+j,f[j]!==!0&&(i+="="+f[j]));return document.cookie=b+"="+e+i}b||(g={});for(var k=document.cookie?document.cookie.split("; "):[],l=/(%[0-9A-Z]{2})+/g,m=0;m<k.length;m++){var n=k[m].split("="),o=n.slice(1).join("=");'"'===o.charAt(0)&&(o=o.slice(1,-1));try{var p=n[0].replace(l,decodeURIComponent);if(o=c.read?c.read(o,p):c(o,p)||o.replace(l,decodeURIComponent),this.json)try{o=JSON.parse(o)}catch(a){}if(b===p){g=o;break}b||(g[p]=o)}catch(a){}}return g}}return d.set=d,d.get=function(a){return d.call(d,a)},d.getJSON=function(){return d.apply({json:!0},[].slice.call(arguments))},d.defaults={},d.remove=function(b,c){d(b,"",a(c,{expires:-1}))},d.withConverter=b,d}return b(function(){})});
+
+}
+/*
+     FILE ARCHIVED ON 14:53:28 Aug 11, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:35:37 Mar 30, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 55.988
+  exclusion.robots: 0.095
+  exclusion.robots.policy: 0.082
+  RedisCDXSource: 0.848
+  esindex: 0.008
+  LoadShardBlock: 32.524 (3)
+  PetaboxLoader3.datanode: 90.784 (4)
+  load_resource: 119.769
+  PetaboxLoader3.resolve: 27.904
+*/
---
+++ web.archive.org/web/20171002181823js_/http_/estrellasdebetta.com/wp-content/plugins/pricing-table/js/site/icona288.js
@@ -0,0 +1,46 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery(function(){
+    var nth = 0, mh = 0, mrh=0, cnt = 0;
+     
+    //jQuery('#shaon-pricing-table').css('width',jQuery('#shaon-pricing-table').parent().width()+'px');
+    /*
+    jQuery('.pt_container_12.responsive_cont').css('max-width','100%');     
+    jQuery('.pt_container_12.responsive_cont .pt_grid_12').css('max-width','100%');     
+    
+    jQuery('.shaon-pricing-table .responsive').css('width',(90/(jQuery('.shaon-pricing-table .responsive').length/jQuery('.shaon-pricing-table').length))+'%');     
+    
+    
+    jQuery('#shaon-pricing-table li img').addClass('aligncenter').css('border','0px').css('padd
+
+}
+/*
+     FILE ARCHIVED ON 18:18:23 Oct 02, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:29:41 Mar 30, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 68.148
+  exclusion.robots: 0.063
+  exclusion.robots.policy: 0.056
+  cdx.remote: 0.041
+  esindex: 0.006
+  LoadShardBlock: 44.335 (3)
+  PetaboxLoader3.datanode: 72.605 (4)
+  load_resource: 695.322
+  PetaboxLoader3.resolve: 661.03
+*/
